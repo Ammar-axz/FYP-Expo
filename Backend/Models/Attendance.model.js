c5895c95199c12e51c2771bb35b14a560a48125b
Maan <abdul4758rehman@gmail.com>
2025-07-26 08:14:35 -0700

    Your meaningful commit message

--- Backend/Models/Attendance.model.js
+++ Backend/Models/Attendance.model.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 <<<<<<< HEAD
 const attendanceSchema = new mongoose.Schema(
     {
-        Student_id:{type:mongoose.Schema.Types.ObjectId,required:[true,"Student id is Required"],ref:"User"},
-        Class_id:{type:mongoose.Schema.Types.ObjectId,required:[true,"Class id is Required"],ref:"Classes"},
+        Student_id:{type:mongoose.Schema.Types.ObjectID,required:[true,"Student id is Required"],ref:"User"},
+        Class_id:{type:mongoose.Schema.Types.ObjectID,required:[true,"Class id is Required"],ref:"Classes"},
         Student_Name:{type:String,required:[true,"Student Name is Required"]},
         Day:{type:String,required:[true,"Day is Required"]},
         Date:{type:Date,required:[true,"Date is Required"]},
