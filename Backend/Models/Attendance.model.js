b07348e6f5b974829f82b1848a0e22c1daa0b307
Ammar-axz <152329278+Ammar-axz@users.noreply.github.com>
2025-07-27 16:32:56 +0500

    attendance model conflict resolved

--- Backend/Models/Attendance.model.js
+++ Backend/Models/Attendance.model.js
@@ -1,34 +1,14 @@
 import mongoose from "mongoose";
 
-<<<<<<< HEAD
 const attendanceSchema = new mongoose.Schema(
     {
-        Student_id:{type:mongoose.Schema.Types.ObjectID,required:[true,"Student id is Required"],ref:"User"},
-        Class_id:{type:mongoose.Schema.Types.ObjectID,required:[true,"Class id is Required"],ref:"Classes"},
+        Student_id:{type:mongoose.Schema.Types.ObjectId,required:[true,"Student id is Required"],ref:"User"},
+        Class_id:{type:mongoose.Schema.Types.ObjectId,required:[true,"Class id is Required"],ref:"Classes"},
         Student_Name:{type:String,required:[true,"Student Name is Required"]},
         Day:{type:String,required:[true,"Day is Required"]},
         Date:{type:Date,required:[true,"Date is Required"]},
         Status:{type:Boolean,required:[true,"Status is Required"]}
     }
 )
-=======
-const attendanceSchema = new mongoose.Schema({
-  Student_id: {
-    type: mongoose.Schema.Types.ObjectID,
-    required: [true, "Student id is Required"],
-    ref: "User",
-  },
-  Class_id: {
-    type: mongoose.Schema.Types.ObjectID,
-    required: [true, "Class id is Required"],
-    ref: "Classes",
-  },
-  Student_Name: { type: String, required: [true, "Student Name is Required"] },
-  Day: { type: String, required: [true, "Day is Required"] },
-  Date: { type: Date, required: [true, "Date is Required"] },
-  Status: { type: Boolean, required: [true, "Status is Required"] },
-});
->>>>>>> cf9716e20fcbd0631038550ef0b782599628d889
-
 const Attendance = new mongoose.model("Attendance", attendanceSchema);
 export default Attendance;
