5b733bf4f1980a0fe28a92cd99d44bbcafff01eb
Maan <abdul4758rehman@gmail.com>
2025-07-28 02:45:05 -0700

    WIP: Editing Attendance model

--- Backend/Models/Attendance.model.js
+++ Backend/Models/Attendance.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+<<<<<<< Updated upstream
 const attendanceSchema = new mongoose.Schema(
     {
         Student_id:{type:mongoose.Schema.Types.ObjectId,required:[true,"Student id is Required"],ref:"User"},
@@ -11,4 +12,36 @@ const attendanceSchema = new mongoose.Schema(
     }
 )
 const Attendance = new mongoose.model("Attendance", attendanceSchema);
+=======
+const attendanceSchema = new mongoose.Schema({
+  Student_id: {
+    type: mongoose.Schema.Types.ObjectID,
+    required: [true, "Student id is Required"],
+    ref: "User",
+  },
+  Class_id: {
+    type: mongoose.Schema.Types.ObjectID,
+    required: [true, "Class id is Required"],
+    ref: "Classes",
+  },
+  Student_Name: { 
+    type: String, 
+    required: [true, "Student Name is Required"] 
+  },
+  Day: { 
+    type: String, 
+    required: [true, "Day is Required"] 
+  },
+  Date: { 
+    type: Date, 
+    required: [true, "Date is Required"] 
+  },
+  Status: { 
+    type: Boolean, 
+    required: [true, "Status is Required"] 
+  },
+});
+
+const Attendance = mongoose.model("Attendance", attendanceSchema);
+>>>>>>> Stashed changes
 export default Attendance;
